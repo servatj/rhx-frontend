50343df1eeca64daffba90c61fe5c05063f81522
servatj <jservatlorca@gmail.com>
2023-10-04 22:14:46 +0400

    add footer test

--- src/components/Footer.test.jsx
+++ src/components/Footer.test.jsx
@@ -1,7 +1,16 @@
-import {describe, test} from 'vitest';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import {describe, test, expect} from 'vitest';
+import Footer from './Footer'; 
 
 describe("Footer test", () => {
-    test("Should show", () => {
+    test("Should show Footer Properly", () => {
+      render(<Footer />);
 
+      expect(screen.getByText('RHX')).toBeInTheDocument();
+      expect(screen.getByText('© 2023 All rights reserved.')).toBeInTheDocument();
+      expect(screen.getByText('Privacy Policy')).toBeInTheDocument();
+      expect(screen.getByText('Terms of Service')).toBeInTheDocument();
+      expect(screen.getByText('Contact Us')).toBeInTheDocument();
     })
 })
