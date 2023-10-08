53eeea02187d87d29895f980ae4016ec8ab1ebc2
servatj <jservatlorca@gmail.com>
2023-10-08 21:23:31 +0400

    clean auth

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,14 +1,43 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 
 const AuthContext = createContext({});
 
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ children }: any) => {
+const login = (email: string, password: string) =>
+  supabase.auth.signInWithPassword({ email, password });
+
+const signOut = () => supabase.auth.signOut();
+
+const passwordReset = (email: string) =>
+  supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "http://localhost:5173/update-password"
+  });
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(false);
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event == "PASSWORD_RECOVERY") {
+        setAuth(false);
+      } else if (event === "SIGNED_IN") {
+        setUser(session.user);
+        setAuth(true);
+      } else if (event === "SIGNED_OUT") {
+        setAuth(false);
+        setUser(null);
+      }
+    });
+    return () => {
+      data.subscription.unsubscribe();
+    };
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ auth, user, login, signOut, passwordReset }}>
       {children}
     </AuthContext.Provider>
   );
