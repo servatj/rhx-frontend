13664ec62fd02e1baa4e20a0d5089cfff30ee0cf
servatj <jservatlorca@gmail.com>
2023-10-22 22:39:41 +0400

    remove the welcome back

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import Container from "../components/Container";
 import FeaturesProfile from "../components/FeaturesProfile";
 import Layout from "../components/Layout";
 
 const ProfilePage: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [])
+
   return (
     <Layout>
-      <h1>Welcome Back 👋</h1>
+      {isVisible && <h1 className="transition-opacity duration-500 pl-2">Welcome Back 👋</h1>}
       <Container className="bg-slate-700">
         <div className={`flex m-8 justify-center items-center `}>
 					<FeaturesProfile />
