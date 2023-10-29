c170188765dda28501ed868058f951924dde1e8b
servatj <jservatlorca@gmail.com>
2023-10-29 08:15:57 +0400

    basic profile

---
+++ src/components/FeatureBox.tsx
@@ -0,0 +1,16 @@
+const FeatureBox = ({ title, description}) => {
+	return (
+    <>    
+      <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg hover:scale-75">
+        <div className="px-4 py-5 flex-auto">
+          <h6 className="text-xl font-semibold">{title}</h6>
+          <p className="mt-2 mb-4 text-blueGray-500">
+            {description}
+          </p>
+        </div>
+      </div>
+    </>
+	);
+};
+
+export default FeatureBox;
--- src/components/FeaturesProfile.tsx
+++ src/components/FeaturesProfile.tsx
@@ -1,19 +1,40 @@
-import FeatureBox from './FeatureBox';
+import {
+	BeakerIcon,
+	AcademicCapIcon,
+	PrinterIcon,
+	UserPlusIcon,
+	WrenchScrewdriverIcon,
+	AdjustmentsVerticalIcon,
+} from "@heroicons/react/24/solid";
+import FeatureBox from "./FeatureBox";
 import { Link } from "react-router-dom";
 
 const Features = () => {
-  return (
-    <section className="">
-    <h2 className="text-2xl font-semibold mb-6 text-left">
-      What you want to do today?
-    </h2>
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-      <Link to='/cover'>
-        <FeatureBox title={'Create a perfect cover letter'} description={'Answer few questions or provide your cv, It can even adapt the tone and style based on the company s culture'}/>
-      </Link>
-    </div>
-  </section>
-  )
-}
+	return (
+		<section className="">
+			<h2 className="text-2xl font-semibold mb-6 text-left">
+				What you want to do today?
+			</h2>
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+				<Link to="/cover">
+					<FeatureBox
+						title={"Create a perfect cover letter"}
+						description={
+							"Answer few questions or provide your cv, It can even adapt the tone and style based on the company s culture"
+						}
+					/>
+				</Link>
+				<FeatureBox
+					title={"Let us enhance your cv -- coming soon"}
+					description={"Let us enhance the quality of your cv"}
+				/>
+				<FeatureBox
+					title={"AI generate portfolio picture -- coming soon"}
+					description={"We care about your personal brand"}
+				/>
+			</div>
+		</section>
+	);
+};
 
 export default Features;
